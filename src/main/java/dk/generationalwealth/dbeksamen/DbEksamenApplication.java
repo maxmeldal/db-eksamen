2baa787aaffb37b3e41a84800e55ba49f0ee4377
maxnorthtech <max@northtech.dk>
2022-09-15 13:15:14 +0200

    Initial commit

---
+++ src/main/java/dk/generationalwealth/dbeksamen/DbEksamenApplication.java
@@ -0,0 +1,13 @@
+package dk.generationalwealth.dbeksamen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbEksamenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbEksamenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dk/generationalwealth/dbeksamen/DbEksamenApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.generationalwealth.dbeksamen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbEksamenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
